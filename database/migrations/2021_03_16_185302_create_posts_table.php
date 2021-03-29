8afc9b203a5cae757dea16a6081bce881622666a
integrity7 <katerina.goncharova777@mail.ru>
2021-03-29 13:57:16 +0300

    message1

--- database/migrations/2021_03_16_185302_create_posts_table.php
+++ database/migrations/2021_03_16_185302_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('post_id');
+            $table->bigIncrements('id');
             $table->bigInteger('author_id')->unsigned();
             $table->string('title');
             $table->string('short_title');
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,45 @@
-<h2>тестовый заголовок</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"></head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class=" container collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="col-6 navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Главная</a>
+        </li>
+        <li class="nav-item offset-3">
+          <a class="nav-link active" aria-current="page" href="/">Создать пост</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Найти пост..." aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Поиск</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <div class="row">
+        @foreach($posts as $post)
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header"><h2>{{ $post->short_title }}</h2></div>
+                <div class="card-body">
+                  <div class="card-img" style="background-image: url({{$post->img ?? asset('img/default.jpg')}});"></div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    {{$posts -> links()}}
+</div>
+</body>
+</html>
